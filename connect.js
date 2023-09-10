ca7dd1002ac120031905b94237de18a773812310
harshit <harshit@zeddlabz.com>
2023-09-10 21:26:22 +0530

    Done

---
+++ connect.js
@@ -0,0 +1,51 @@
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+dotenv.config();
+
+// export const db = mysql.createConnection({
+//   host: process.env.HOST,
+//   user: process.env.USER,
+//   password: process.env.PASSWORD,
+//   database: process.env.DATABASE,
+// });
+
+var db_config = {
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+};
+
+var db;
+
+function handleDisconnect() {
+  db = mysql.createConnection(db_config); // Recreate the connection, since
+  // the old one cannot be reused.
+
+  db.connect(function (err) {
+    // The server is either down
+    if (err) {
+      // or restarting (takes a while sometimes).
+      console.log("error when connecting to db:", err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    } // to avoid a hot loop, and to allow our node script to
+    else{
+      console.log("Database Connect Successfully");
+    }
+  }); // process asynchronous requests in the meantime.
+  // If you're also serving http, display a 503 error.
+  db.on("error", function (err) {
+    console.log("db error", err);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      // Connection to the MySQL server is usually
+      handleDisconnect(); // lost due to either server restart, or a
+    } else {
+      // connnection idle timeout (the wait_timeout
+      throw err; // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
+
+module.exports = { db };
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // import dotenv from "dotenv";
 const express = require("express");
 const path = require("path");
+const { db } = require("./connect.js")
 
 
 const app = express();
@@ -13,6 +14,17 @@ app.get("/try", (req, res) => {
   res.send(process.env.VARIABLE);
 });
 
+app.get("/trial", (req, res) => {
+    const q = 'INSERT INTO name(`roll`) VALUES ("Hello")';
+  
+    db.query(q, (err, data) => {
+      if (err) return res.status(500).json(err);
+      console.log("Success");
+      // return res.status(200).json("Sociodemographic Survey has been sent succesfully");
+    });
+    res.send(" Hello");
+  });
+  
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./build/index.html"));
