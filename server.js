ab065410edbb5a6ddf98ce95dd82171cb043fed7
harshit <harshit@zeddlabz.com>
2023-09-10 20:23:50 +0530

    Done

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 // import express from "express"
 // import dotenv from "dotenv";
-const express = require("express")
+const express = require("express");
+const path = require("path");
+
 
 const app = express();
 
 require("dotenv").config();
+app.use(express.static(path.join(__dirname, "./build")));
 
-app.get("/", (req,res)=>{
-    res.send(process.env.VARIABLE)
-})
+app.get("/try", (req, res) => {
+  res.send(process.env.VARIABLE);
+});
 
-app.listen(5000, ()=>{
-    console.log("Server running at 5000");
-})
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./build/index.html"));
+});
+
+app.listen(5000, () => {
+  console.log("Server running at 5000");
+});
